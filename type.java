7ec0a2704bc014cd19393c1d6720b2515c6dccd7
SHABBIRS786 <121858329+SHABBIRS786@users.noreply.github.com>
2023-01-09 16:19:35 +0530

    Add files via upload

---
+++ type.java
@@ -0,0 +1,11 @@
+class Type
+ {
+   public static void main(String[] args)
+   {
+     char letter='a';
+     int ascii=letter;
+     System.out.println("ASCII value of="+letter+"is:"+ascii);
+     char ex=(char)(ascii-32);
+     System.out.println("from ASCII value to="+ascii+"letter is:"+ex);
+   }
+ }
